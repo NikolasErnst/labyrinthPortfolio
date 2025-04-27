3e439deefa0bbf236c701206484ea1b5a1387290
NikolasErnst <148749813+NikolasErnst@users.noreply.github.com>
2025-04-27 23:47:12 +0200

    Merge remote-tracking branch 'origin/master'

--- SEPortfolioLabyrinth/src/main/java/com/example/SEPortfolioLabyrinth/SePortfolioLabyrinthApplication.java
+++ SEPortfolioLabyrinth/src/main/java/com/example/SEPortfolioLabyrinth/SePortfolioLabyrinthApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SePortfolioLabyrinthApplication {
-//test 123
 	public static void main(String[] args) {
 		SpringApplication.run(SePortfolioLabyrinthApplication.class, args);
 	}
