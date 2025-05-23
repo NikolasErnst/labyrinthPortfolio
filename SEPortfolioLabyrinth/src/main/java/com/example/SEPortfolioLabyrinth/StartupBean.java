3a6714c37f5eb759109a4817d8ed6c8d15b07e0d
Nikolas Ernst <nikolasernst99@gmail.com>
2025-05-23 15:41:52 +0200

    Init Phase 3 (Applikation die erfolgreich das Maze-Game auf dem Server spielt)

--- SEPortfolioLabyrinth/src/main/java/com/example/SEPortfolioLabyrinth/StartupBean.java
+++ SEPortfolioLabyrinth/src/main/java/com/example/SEPortfolioLabyrinth/StartupBean.java
@@ -4,7 +4,9 @@ import jakarta.annotation.PostConstruct;
 import org.openapitools.client.api.DefaultApi;
 import org.openapitools.client.model.GameDto;
 import org.openapitools.client.model.GameInputDto;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StartupBean {
 
     @PostConstruct
